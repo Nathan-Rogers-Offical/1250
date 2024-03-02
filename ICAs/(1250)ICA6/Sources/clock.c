a073a77011ee32ebc929f2fd9c1eb8df2ef07cd1
Nathan-Rogers-Offical <148917066+Nathan-Rogers-Offical@users.noreply.github.com>
2024-03-01 21:18:38 -0700

    Add files via upload

---
+++ ICAs/(1250)ICA6/Sources/clock.c
@@ -0,0 +1,34 @@
+#include <hidef.h>      /* common defines and macros */
+#include "derivative.h" /* derivative-specific definitions */
+#include "clock.h"
+
+void Clock_Set24MHZ(void) {
+    SYNR = 5;
+    REFDV = 3;
+    CLKSEL_PSTP = 1;
+    PLLCTL = 0b01100000;
+    while(!CRGFLG_LOCK);
+    CLKSEL_PLLSEL = 1;
+}
+
+void Clock_Set20MHZ(void) {
+    SYNR = 4;
+    REFDV = 3;
+    CLKSEL_PSTP = 1;
+    PLLCTL = 0b01100000;
+    while (!CRGFLG_LOCK);
+    CLKSEL_PLLSEL = 1;
+}
+
+void Clock_Set8MHZ(void) {
+    SYNR = 1;
+    REFDV = 3;
+    CLKSEL_PSTP = 1;
+    PLLCTL = 0b01100000;
+    while (!CRGFLG_LOCK);
+    CLKSEL_PLLSEL = 1;
+}
+
+unsigned long Clock_GetBusSpeed(){
+
+}
